81a6061ea830300189847bb1d57dc93751e77e9d
samuelconlon <conlonsp@yahoo.com>
2025-01-13 19:25:07 -0500

    commit tree f140afb8df815d5ecdbd4bc0b5ff305d272ea2c0

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector("main")
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const li = document.createElement("li")
+    li.textContent = book.name
+    main.appendChild(li)
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener("DOMContentLoaded", () => {
+  fetchBooks()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then(renderBooks)
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
